9360ca77c1b545c5a7b5077678b4efeb0eed7b9e
Jasper Kent <software@jasperkent.com>
2022-12-02 09:25:53 +0000

    Add project files.

---
+++ RequiredMembers/Person.cs
@@ -0,0 +1,22 @@
+﻿using System.Diagnostics.CodeAnalysis;
+
+namespace RequiredMembers
+{
+    public class Person
+    {
+        public required string Name { get; init; }
+        public required int Age { get; set; }
+
+        [SetsRequiredMembers]
+        public Person(string name, int age)
+        {
+            Name = name;
+            Age = age;
+        }
+
+        public override string ToString()
+        {
+            return $"{Name} ({Age})";
+        }
+    }
+}
---
+++ RequiredMembers/Program.cs
@@ -0,0 +1,7 @@
+﻿using RequiredMembers;
+
+var fred = new Person("Fred Flintstone", 41);
+
+//fred.Age = 35;
+
+Console.WriteLine(fred);
